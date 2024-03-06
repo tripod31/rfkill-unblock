d72d89519a2694cff051049bb5619420e1a70ce5
tripod31 <tripod31@hotmail.com>
2024-03-06 11:17:34 +0900

    ok

--- test.py
+++ test.py
@@ -1,37 +1,56 @@
 #!/usr/bin/env python3
 # coding:utf-8
 
-import unittest
-import subprocess
+import time
+import os
+from stat import *
 
-class Test1(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        pass
+class Process:
+    def __init__(self):
+        self.path           = "/var/log/syslog"     #監視対象ファイル
+        self.keyword        = "rfkill-unblock.py"   #監視対象キーワード
+        self.interval_sec   = 1                     #ファイルを監視する秒間隔
+        self.timeout_sec    = 70                    #タイムアウトする秒数
 
-    def test1(self):
+        self.file = open(self.path,'r')
+
+        #ファイル末尾へ移動
+        st_results = os.stat(self.path)
+        st_size = st_results[ST_SIZE]
+        self.file.seek(st_size)
+
+        self.start_time = time.time()  # 開始時刻
+
+    def __del__(self):
+        self.file.close()
+
+    def tail_f(self):
         """
-        syslogを監視して、スクリプトが実行されていることを確認
+        ファイルの末尾を監視
         """
-        cmd = "timeout 70 tail -n 0 -f /var/log/syslog|grep rfkill-unblock.py"
-        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        output=""
-        while proc.poll() is None:
-            output = proc.stdout.readline().decode().strip()
-            print(output)
-            proc.kill()
-            proc.wait()
-            break
+        while True:
+            fpos = self.file.tell()
+            line = self.file.readline()
+            if not line:
+                time.sleep(self.interval_sec)
+                self.file.seek(fpos)    #スリープ前の位置に戻す
 
-        if len(output)>0:
-            print("syslogでスクリプトの出力が確認できました")
-        else:
-            print("syslogでスクリプトの出力が確認できませんでした")
-        assert len(output)>0
+                #タイムアウトチェック
+                now = time.time()
+                diff = int(now - self.start_time)
+                print(f"\r{diff:02}秒経過",end="",flush=True)
+                if diff > self.timeout_sec:
+                    print(f"\nsyslogに{self.keyword}が確認できませんでした")
+                    break
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
+                continue
+
+            if line.find(self.keyword)>0:
+                print(f"\nsyslogに{self.keyword}を確認しました")
+                print(line.strip())
+                break
+
 
 if __name__ == '__main__':
-    unittest.main()
+    proc = Process()
+    proc.tail_f()
