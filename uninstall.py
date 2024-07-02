cd1c2d7ba56de8dffbea772c8ee8a525aa1d31bd
tripod31 <tripod31@hotmail.com>
2024-07-02 21:44:56 +0900

    fix

--- uninstall.py
+++ uninstall.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import os
-import shutil
+import subprocess
 
 def uninstall():
     if os.path.exists("/usr/local/bin/rfkill-unblock.py"):
         os.remove("/usr/local/bin/rfkill-unblock.py")
 
     if os.path.exists("/etc/systemd/system/rfkill-unblock.service"):
-        subsubprocess.run("systemctl stop rfkill-unblock.service",shell=True) 
-        subsubprocess.run("systemctl disable rfkill-unblock.service",shell=True)
+        subprocess.run("systemctl stop rfkill-unblock.service",shell=True) 
+        subprocess.run("systemctl disable rfkill-unblock.service",shell=True)
         os.remove("/etc/systemd/system/rfkill-unblock.service")
 
 if __name__ == '__main__':
